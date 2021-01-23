f647b20e119e8cd27c2bab36f4bdf4bffb8065d0
Csaba Sandor <csabull96@gmail.com>
2021-01-23 10:57:02 +0200

    basic implementation of the HandDetector class

---
+++ HandDetector/Detector/IHandDetector.cs
@@ -0,0 +1,15 @@
+﻿using Caduhd.HandDetector.Model;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caduhd.HandDetector.Detector
+{
+    interface IHandDetector
+    {
+        Hands DetectHands(Bitmap frame);
+    }
+}
---
+++ HandDetector/Model/Hands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caduhd.HandDetector.Model
+{
+    public class Hands
+    {
+        public Hand Left { get; private set; }
+        public Hand Right { get; private set; }
+
+        public Hands(Hand left, Hand right)
+        {
+            Left = left;
+            Right = right;
+        }
+    }
+}
